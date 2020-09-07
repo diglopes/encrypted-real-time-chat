3193c88072e6880676bbe9faae79efdfc642d0e5
Diego Lopes <diego-chez@hotmail.com>
2020-09-07 08:17:17 -0300

    feat(server): add new user

--- packages/server/src/model/__test__/dummy-user.spec.ts
+++ packages/server/src/model/__test__/dummy-user.spec.ts
@@ -6,4 +6,16 @@ describe('Dummy User', () => {
     const users = dummyUser.get();
     expect(users).toEqual([]);
   });
+
+  it('should add a new user', () => {
+    const user = {
+      id: 'fake-id',
+      username: 'any-username',
+      room: 'fake-room',
+    };
+    const dummyUser = new DummyUser();
+    const result = dummyUser.add(user);
+    expect(dummyUser.get()).toEqual([user]);
+    expect(result).toBeTruthy();
+  });
 });
--- packages/server/src/model/dummy-user.ts
+++ packages/server/src/model/dummy-user.ts
@@ -1,7 +1,18 @@
-export class DummyUser {
-  constructor(private users: [] = []) {}
+export interface User {
+  id: string;
+  username: string;
+  room: string;
+}
 
-  public get(): [] {
+export class DummyUser {
+  constructor(private users: Array<User> = []) {}
+
+  public get(): Array<User> {
     return this.users;
+  }
+
+  public add(user: User): boolean {
+    this.users.push(user);
+    return true;
   }
 }
